9e493a97ed798500ad918a2e1b1a3c9fbbba457d
rezanicgil <rezanicgil@gmail.com>
2024-06-09 23:46:38 +0300

    feat: securing .env credentials

--- backend/index.ts
+++ backend/index.ts
@@ -4,9 +4,10 @@ dotenv.config()
 import Server from './src/index'
 
 const app: Application = express()
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const server: Server = new Server(app)
 
-const PORT = process.env.NODE_DOCKER_PORT || 8080;
+const PORT = process.env.NODE_DOCKER_PORT || 8080
 
 app.listen(PORT, async () => {
   console.log(`Server is running at http://localhost:${PORT}`)
